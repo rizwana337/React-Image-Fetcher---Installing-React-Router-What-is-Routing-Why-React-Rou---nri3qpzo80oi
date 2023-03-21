a3f174e3c2b53262acccd362010e9d7f8b87af58
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-03-21 22:38:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
+  const[loading, setLoading] = useState(false);
+  const[id, setId] = useState("");
+  const[item, setItem] = useState({});
+  const[state, setState] = useState(false);
   
+    const getData = async ()=>{
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/photos/"+id);
+            const data = await response.json();
+            setItem(data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+  const handleChange = (e)=>{
+    setState(true);
+    setLoading(true) ;
+    if(!e.target.value || e.target.value<=0){
+        setState(false)
+    }
+    setId(e.target.value);
+    
+  }
+
+  useEffect(()=>{
+    getData();
+  }, [id]);
+    return(
+        <div className='app'>
+            id<input type="number" onChange={handleChange}/><br />
+            {state? (loading? <Loader /> : <PhotoFrame title={item.title} url={item.url} />) : null}
+        </div>
+    )
 }
 
 
