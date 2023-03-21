739efae8459b2f503862a4eb396129c47ca796b1
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-03-21 23:46:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
-const App = () => {
-  const[loading, setLoading] = useState(false);
-  const[id, setId] = useState("");
-  const[item, setItem] = useState({});
-  const[state, setState] = useState(false);
-  
-    const getData = async ()=>{
-        try {
-            const response = await fetch("https://jsonplaceholder.typicode.com/photos/"+id);
-            const data = await response.json();
-            setItem(data);
-            setLoading(false);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-  const handleChange = (e)=>{
-    setState(true);
-    setLoading(true) ;
-    if(!e.target.value || e.target.value<=0){
-        setState(false)
-    }
-    setId(e.target.value);
-    
+import React, { useEffect, useReducer } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { PhotoFrame } from "./PhotoFrame";
+
+const reducerFunction = (state, action) => {
+  if (action.type === "fetch") {
+    return {
+      title: action.title,
+      url: action.url,
+      isLoading: state.isLoading,
+      id: state.id,
+    };
   }
+  if (action.type === "trigger") {
+    return {
+      title: state.title,
+      url: state.url,
+      isLoading: state.isLoading,
+      id: action.id,
+    };
+  }
+  if (action.type === "startLoading") {
+    return {
+      title: state.title,
+      url: state.url,
+      isLoading: true,
+      id: state.id,
+    };
+  }
+  if (action.type === "stopLoading") {
+    return {
+      title: state.title,
+      url: state.url,
+      isLoading: false,
+      id: state.id,
+    };
+  }
+  return state;
+};
 
-  useEffect(()=>{
-    getData();
-  }, [id]);
-    return(
-        <div className='app'>
-            id<input type="number" onChange={handleChange}/><br />
-            {state? (loading? <Loader /> : <PhotoFrame title={item.title} url={item.url} />) : null}
-        </div>
-    )
-}
+const App = () => {
+  const [apiData, dispatch] = useReducer(reducerFunction, {
+    title: "",
+    url: "",
+    isLoading: false,
+    id: null,
+  });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "startLoading" });
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos/${apiData.id}`
+      );
+      const data = await response.json();
+      dispatch({ type: "stopLoading" });
+      dispatch({ type: "fetch", title: data.title, url: data.url });
+
+      console.log(data);
+    };
+    if (apiData.id !== null && apiData.id !== "") {
+      fetchData();
+    }
+  }, [apiData.id]);
+
+  const inputChangeHandler = (e) => {
+    dispatch({ type: "trigger", id: e.target.value });
+  };
+  return (
+    <>
+      <label htmlFor="idNumber">Id number</label>
+      <input id="idNumber" type="number" onChange={inputChangeHandler} />
+      {apiData.id !== null && !apiData.isLoading && (
+        <PhotoFrame url={apiData.url} title={apiData.title} />
+      )}
+      {apiData.isLoading && <Loader />}
+    </>
+  );
+};
 
 export default App;
